1479c9d960742e8eac8111849f708d15947cac41
Jannis Fuchs <janifu02@gmail.com>
2025-09-30 17:29:51 +0200

    dynamic updated of blog overview

--- src/components/blog.js
+++ src/components/blog.js
@@ -4,11 +4,21 @@ import "../stylesheets/global.css"
 function Blog() {
     const [searchParams] = useSearchParams();
     const week=searchParams.get("week")
-
-    return(
+    
+    if(/week[1-9]+/.test(week)){
+        return(
         <div className="container">
             <iframe className="mainComponent" src={`/blog/compiledOrgFiles/${week}.html`} title="Startseite"></iframe>
         </div>
-    );
+        );
+    }
+    else {
+        return(
+        <div className="container">
+            <iframe className="mainComponent" src={`/blog/compiledOrgFiles/404Page.html`} title="Startseite"></iframe>
+        </div>
+        );
+    }
+    
 }
 export default Blog;
--- src/components/blogoverview.js
+++ src/components/blogoverview.js
@@ -1,18 +1,39 @@
+
+import { useState, useEffect } from 'react';
 function BlogOverview() {
-    const postname = ["week1", "week2", "week3"];
+    const [weeks, setWeeks] = useState([]);
+
+    useEffect(() =>{ fetch("/blog/NumberOfFiles.txt")
+        .then(response=> {
+            return response.text();
+        })
+        .then(text =>{
+            const string = text;
+            const count =parseInt(string);
+            const weeksArray = Array.from({ length: count }, (_, i) => `week${i + 1}`);
+            setWeeks(weeksArray);
+        })
+        .catch(error => console.error('Error fetching count:', error));
+    }, []);
+    
     return (
         <div className="container">
             <div className="mainComponent">
                 <h1>Blog Overview</h1>
                 <ul>
-                    {postname.map((post) => (
+                    {weeks.length > 0 ? (
+                        weeks.map(post => (
                         <li key={post}>
                             <a href={`/blog/#/week?week=${post}`}>{post}</a>
                         </li>
-                    ))}
+                        ))
+                    ) : (
+                        <li>No weeks available</li>
+                    )}
                 </ul>
             </div>
         </div>
     );
 }
+
 export default BlogOverview;
